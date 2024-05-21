334ae3fd0b797666cceddddcc9444cd1a8ebc89d
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2024-05-20 19:21:17 -0600

    removed href

--- src/components/commands/Email.tsx
+++ src/components/commands/Email.tsx
@@ -1,18 +1,8 @@
-import { useContext } from "react";
-import _ from "lodash";
 import { Wrapper } from "../styles/Output.styled";
-import { termContext } from "../Terminal";
 import { Link } from "../styles/Welcome.styled";
 
 const Email: React.FC = () => {
-  const { history, rerender } = useContext(termContext);
-
   /* ===== get current command ===== */
-  const currentCommand = _.split(history[0], " ");
-
-  if (rerender && currentCommand[0] === "email" && currentCommand.length <= 1) {
-    window.open("mailto:" + "hello@horumy.dev", "_self");
-  }
 
   return (
     <Wrapper>
